cab0a10f874310dd507269231873ee30a657bfe2
mattfreire <freire1996@gmail.com>
2019-10-29 19:53:58 +0200

    Views

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # third party imports
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from .serializers import PostSerializer
 from .models import Post
@@ -27,3 +29,30 @@ class TestView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+class PostView(
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
-from core.views import TestView
+from core.views import PostView, PostCreateView, PostListCreateView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('admin/', admin.site.urls),
-    path('', TestView.as_view(), name='test'),
+    path('', PostView.as_view(), name='test'),
+    path('create/', PostCreateView.as_view(), name='create'),
+    path('list-create/', PostListCreateView.as_view(), name='list-create'),
     path('api/token/', obtain_auth_token, name='obtain-token')
 ]
